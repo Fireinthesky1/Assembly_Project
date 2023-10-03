ce3048d65e25faa2f62c829a3584b97ae61b5bce
James Hicks <97328702+Fireinthesky1@users.noreply.github.com>
2023-10-02 21:09:32 -0700

    Almost Done with Lab 3

--- Lab3.asm
+++ Lab3.asm
@@ -5,6 +5,7 @@
 
 GPIO_UNLOCK_CODE:       .field      0x4C4F434B
 SYSCTRL_RCGCGPIO_R:     .field      0x400FE608
+SYSCTRL_RCC_R           .field      0x400FE060
 
 GPIO_PORTB_LOCK_R:      .field      0x40005520
 GPIO_PORTB_CR:          .field      0x40005524
@@ -17,9 +18,12 @@ GPIO_PORTF_CR:          .field      0x40025524
 GPIO_PORTF_PUR_R:       .field      0x40025510
 GPIO_PORTF_DEN_R:       .field      0x4002551C
 
+oDATA_READ_BOTH         .field      0x40025044
 oDATA_READ_SW1:         .field      0x40025040
 oDATA_READ_SW2:         .field      0x40025004
 oDATA_WRITE:            .field      0x4000503C
+
+NEITHER_PRESSED:        .equ        0x00010001
 
 main:
         .asmfunc
@@ -79,6 +83,12 @@ main:
         AND     R0, #0
         AND     R1, #0
 
+    ; ENABLE MOSC FOR SYSTEM CLOCK (254)
+        LDR     R0, SYSCTRL_RCC_R
+        LDR     R1, [R0]
+        ORR     R1, #0x30
+        STR     R1, [R0]
+
     ; INITIALIZE NUMBER TO DISPLAY
         MOVT    R0, #0x0000
         MOVB    R0, #0x0000
@@ -91,16 +101,19 @@ DISPLAY:
         AND     R2, #0
         B POLL_SWITCHES
 
+; R2 holds the combined switch data
+; R3 holds the result of the XOR
 POLL_SWITCHES:
-        LDR     R1, oDATA_READ_SW1
+        LDR     R1, oDATA_READ_BOTH
         LDR     R2, [R1]
-        CMP     R2, #0
-        BEQ     COMPLEMENT
-        LDR     R1, oDATA_READ_SW2
-        LDR     R2, [R1]
-        CMP     R2, #0
-        BEQ     SHIFT_LEFT
-        B       DISPLAY
+        EOR     R3, R2, #NEITHER_PRESSED
+        CMP     R3, #0
+        BLNE    wait                        ; if either are pressed wait
+        CMP     R3, #1
+        BEQ     SHIFT_LEFT                  ; if sw2 shift
+        CMP     R3, #0x10
+        BEQ     COMPLEMENT                  ; if sw1 complement
+        B       POLL_SWITCHES
 
 COMPLEMENT:
         MOVT    R1, #0xFFFF
@@ -116,9 +129,64 @@ SHIFT_LEFT:
         .endasmfunc
         .align
 
-wait:   .asmfunc
+; We want to drive the system clock directly from MOSC
+; We need to change bits in System control register RCC
+; R4, R5, R6 are used here
 
+STCTRL:     .field      0xE000E010
+STRELOAD:   .field      0xE000E014
+STCURRENT:  .field      0xE000E018
+COUNTFLAG:  .field      0x00010000
+
+wait:
+        .asmfunc
+
+    ; DISABLE THE COUNTER
+        LDR     R4, STCTRL
+        LDR     R5, [R4]
+        MOV     R6, #1
+        BIC     R5, R5, R6 ; this clears bit zero
+        STR     R5, [R4]
+
+        AND     R4, #0
+        AND     R5, #0
+        AND     R6, #0
+
+    ; SET STRELOAD
+        LDR     R4, STRELOAD
+        LDR     R5, [R4]
+        AND     R5, #0xFF000000
+        MOVT    R6, #0x0002
+        MOVB    R6, #0x7100
+        ORR     R5, R6
+
+        AND     R4, #0
+        AND     R5, #0
+        AND     R6, #0
+
+    ; CLEAR THE COUNTER
+        LDR     R4, STCURRENT
+        AND     R5, #1
+        STR     R5, [R4]
+
+        AND     R4, #0
+        AND     R5, #0
+        AND     R6, #0
+
+    ; START THE TIMER
+        LDR     R4, STCTRL
+        LDR     R5, [R4]
+        ORR     R5, #1
+        STR     R5, [R4]
+
+        LDR     R6, COUNTFLAG
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; THIS PART NEEDS WORK
 waitLoop:
+        LDR     R4, STCTRL
+        LDR     R5, [R5]
+        CMP     R5, R6
         BEQ waitLoop
         BX LR
 
