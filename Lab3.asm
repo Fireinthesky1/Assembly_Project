7f400798b47ffe9625ab0b9df4bb87ad57acd30f
James Hicks <97328702+Fireinthesky1@users.noreply.github.com>
2023-10-03 22:33:57 -0700

    Cleanup and tightening

--- Lab3.asm
+++ Lab3.asm
@@ -3,99 +3,134 @@
         .thumb
         .global main
 
-GPIO_UNLOCK_CODE:       .field      0x4C4F434B
-SYSCTRL_RCGCGPIO_R:     .field      0x400FE608
-SYSCTRL_RCC_R           .field      0x400FE060
+GPIO_UNLOCK_CODE:       .field      0x4C4F434B  ; (pg 684)
+SYSCTRL_RCGCGPIO_R:     .field      0x400FE608  ; (pg 340)
+SYSCTRL_RCC_R           .field      0x400FE060  ; (pg 254)
 
-GPIO_PORTB_LOCK_R:      .field      0x40005520
-GPIO_PORTB_CR:          .field      0x40005524
-GPIO_PORTB_DIR_R:       .field      0x40005400
-GPIO_PORTB_DR4R_R:      .field      0x40005504
-GPIO_PORTB_DEN_R:       .field      0x4000551C
+GPIO_PORTB_LOCK_R:      .field      0x40005520  ; (pg 684)
+GPIO_PORTB_CR:          .field      0x40005524  ; (pg 685)
+GPIO_PORTB_DIR_R:       .field      0x40005400  ; (pg 663)
+GPIO_PORTB_DR4R_R:      .field      0x40005504  ; (pg 674)
+GPIO_PORTB_DEN_R:       .field      0x4000551C  ; (pg 682)
 
-GPIO_PORTF_LOCK_R:      .field      0x40025520
-GPIO_PORTF_CR:          .field      0x40025524
-GPIO_PORTF_PUR_R:       .field      0x40025510
-GPIO_PORTF_DEN_R:       .field      0x4002551C
+GPIO_PORTF_LOCK_R:      .field      0x40025520  ; (pg 684)
+GPIO_PORTF_CR:          .field      0x40025524  ; (pg 685)
+GPIO_PORTF_PUR_R:       .field      0x40025510  ; (pg 677)
+GPIO_PORTF_DEN_R:       .field      0x4002551C  ; (pg 682)
 
-oDATA_READ_BOTH         .field      0x40025044
-oDATA_READ_SW1:         .field      0x40025040
-oDATA_READ_SW2:         .field      0x40025004
-oDATA_WRITE:            .field      0x4000503C
+oDATA_READ_BOTH         .field      0x40025044  ; (pg 662) bit 0,4
+oDATA_READ_SW1:         .field      0x40025040  ; (pg 662) bit 4
+oDATA_READ_SW2:         .field      0x40025004  ; (pg 662) bit 0
+oDATA_WRITE:            .field      0x4000503C  ; (pg 662) bit 0-4
 
 NEITHER_PRESSED:        .equ        0x00010001
 
 main:
         .asmfunc
 
+        AND     R0, #0
+        AND     R1, #0
+
     ; ENABLE CLOCK TO PORT B,F (bit 1, 5)
         LDR     R0, SYSCTRL_RCGCGPIO_R
         LDR     R1, [R1]
-        ORR     R1, #0x22
+        ORR     R1, #0x22                       ; (bit 1 and 5 high)
         STR     R1, [R0]
 
-    ; UNLOCK GPIO PORT B,F
+        AND     R0, #0
+        AND     R1, #0
+
+    ; UNLOCK GPIO PORT B
         LDR     R0, GPIO_PORTB_LOCK_R
         LDR     R1, GPIO_UNLOCK_CODE
         STR     R1, [R0]
+
+        AND     R0, #0
+        AND     R1, #0
+
+    ; UNLOCK GPIO PORT F
         LDR     R0, GPIO_PORTF_LOCK_R
+        LDR     R1, GPIO_UNLOCK_CODE
         STR     R1, [R0]
+
+        AND     R0, #0
+        AND     R1, #0
 
     ; ENABLE COMMIT FOR PORT B, F
         LDR     R0, GPIO_PORTB_CR
         LDR     R1, [R0]
-        ORR     R1, #0x0F
+        ORR     R1, #0x0F                       ; enable commit for pin 0-4
         STR     R1, [R0]
+
+        AND     R0, #0
+        AND     R1, #0
+
         LDR     R0, GPIO_PORTF_CR
         LDR     R1, [R0]
-        ORR     R1, #0x11
+        ORR     R1, #0x11                       ; enable commit for pin 0,4
         STR     R1, [R0]
+
+        AND     R0, #0
+        AND     R1, #0
 
     ; SET DIRECTION (PB0-4 outputs)
         LDR     R0, GPIO_PORTB_DIR_R
         LDR     R1, [R0]
-        ORR     R1, #0x0F
+        ORR     R1, #0x0F                       ; set PB0-4 as output
         STR     R1, [R0]
+
+        AND     R0, #0
+        AND     R1, #0
 
     ; SET DRIVE STRENGTH FOR PORTB to 4-mA
         LDR     R0, GPIO_PORTB_DR4R_R
         LDR     R1, [R0]
-        ORR     R1, #0x0F
+        ORR     R1, #0x0F                       ; pins 0-4 have 4-mA drive
         STR     R1, [R0]
+
+        AND     R0, #0
+        AND     R1, #0
 
     ; ENABLE PULL UP RESISTOR FOR PF0, PF4
         LDR     R0, GPIO_PORTF_PUR_R
         LDR     R1, [R0]
-        ORR     R1, #0x11
+        ORR     R1, #0x11                       ; set PUR for PF0 and PF4
         STR     R1, [R0]
 
-    ; SET DIGITAL ENABLE (PB0-4, PF0, PF4)
-        LDR     R0, GPIO_PORTB_DEN_R
-        LDR     R1, [R0]
-        ORR     R1, #0x0F
-        STR     R1, [R0]
-        LDR     R0, GPIO_PORTF_DEN_R
-        LDR     R1, [R0]
-        ORR     R1, #0x11
-        STR     R1, [R0]
-
-    ; CLEAR REGISTERS
         AND     R0, #0
         AND     R1, #0
 
-    ; ENABLE MOSC FOR SYSTEM CLOCK (254)
-        LDR     R0, SYSCTRL_RCC_R
+    ; SET DIGITAL ENABLE (PB0-4, PF0, PF4) <- CHECK THIS
+        LDR     R0, GPIO_PORTB_DEN_R
         LDR     R1, [R0]
-        ORR     R1, #0x30
+        ORR     R1, #0x0F                       ; enable for pin 0-4
         STR     R1, [R0]
 
-    ; INITIALIZE NUMBER TO DISPLAY
-        MOVT    R0, #0x0000
-        MOVB    R0, #0x0000
+        AND     R0, #0
+        AND     R1, #0
 
-DISPLAY:
-        LDR     R1, oDATA_WRITE
-        MOVB    R2, R0
+        LDR     R0, GPIO_PORTF_DEN_R
+        LDR     R1, [R0]
+        ORR     R1, #0x11                       ; enable for pin 0,4
+        STR     R1, [R0]
+
+        AND     R0, #0
+        AND     R1, #0
+
+    ; ENABLE MOSC FOR SYSTEM CLOCK (254; bit 5 and 4 need to be 0)
+        LDR     R0, SYSCTRL_RCC_R
+        LDR     R1, [R0]
+        EOR     R1, #0x30                       ; set bit 5 and 4 to 0
+        STR     R1, [R0]
+
+    ; CLEAR R1 AND INITIALIZE R0 FOR DISPLAY
+        AND     R0, #0
+        AND     R1, #0
+        AND     R2, #0
+
+DISPLAY: ; check with prof to see if this works
+        LDR     R1, oDATA_WRITE                 ; load address for writing
+        UBFX    R2, R0, #0, #4                  ; unsigned bitfield extract
         STR     R2, [R1]
         AND     R1, #0
         AND     R2, #0
@@ -103,16 +138,19 @@ DISPLAY:
 
 ; R2 holds the combined switch data
 ; R3 holds the result of the XOR
+; If neither are pressed: 0x11
+; If SW1 is pressed: 0x01
+; If SW2 is pressed: 0x10
 POLL_SWITCHES:
         LDR     R1, oDATA_READ_BOTH
         LDR     R2, [R1]
         EOR     R3, R2, #NEITHER_PRESSED
         CMP     R3, #0
-        BLNE    wait                        ; if either are pressed wait
+        BLNE    wait                            ; if either wait
         CMP     R3, #1
-        BEQ     SHIFT_LEFT                  ; if sw2 shift
+        BEQ     SHIFT_LEFT                      ; if sw2 shift
         CMP     R3, #0x10
-        BEQ     COMPLEMENT                  ; if sw1 complement
+        BEQ     COMPLEMENT                      ; if sw1 complement
         B       POLL_SWITCHES
 
 COMPLEMENT:
@@ -133,32 +171,44 @@ SHIFT_LEFT:
 ; We need to change bits in System control register RCC
 ; R4, R5, R6 are used here
 
-STCTRL:     .field      0xE000E010
-STRELOAD:   .field      0xE000E014
-STCURRENT:  .field      0xE000E018
-COUNTFLAG:  .field      0x00010000
+STCTRL:     .field      0xE000E010              ; (pg 138)
+STRELOAD:   .field      0xE000E014              ; (pg 140)
+STCURRENT:  .field      0xE000E018              ; (pg 141)
+COUNTFLAG:  .field      0x00010000              ; (pg 138)
 
 wait:
         .asmfunc
-
-    ; DISABLE THE COUNTER
-        LDR     R4, STCTRL
-        LDR     R5, [R4]
-        MOV     R6, #1
-        BIC     R5, R5, R6 ; this clears bit zero
-        STR     R5, [R4]
 
         AND     R4, #0
         AND     R5, #0
         AND     R6, #0
 
-    ; SET STRELOAD
+    ; DISABLE THE COUNTER (bit 0 low)
+        LDR     R4, STCTRL
+        LDR     R5, [R4]
+        EOR     R5, #1                          ; set bit 0 low
+        STR     R5, [R4]
+
+        AND     R4, #0
+        AND     R5, #0
+
+    ; USE SYSTEM CLOCK (bit 2 high)
+        LDR     R4, STCTRL
+        LDR     R5, [R4]
+        ORR     R5, #4                          ; set bit 4 high
+        STR     R5, [R4]
+
+        AND     R4, #0
+        AND     R5, #0
+
+    ; SET STRELOAD TO 0x27100
+    ; be careful here ORR r5, R6 might now be correct
         LDR     R4, STRELOAD
         LDR     R5, [R4]
-        AND     R5, #0xFF000000
         MOVT    R6, #0x0002
         MOVB    R6, #0x7100
         ORR     R5, R6
+        STR     R5, [R4]
 
         AND     R4, #0
         AND     R5, #0
@@ -166,28 +216,27 @@ wait:
 
     ; CLEAR THE COUNTER
         LDR     R4, STCURRENT
-        AND     R5, #1
-        STR     R5, [R4]
+        AND     R5, #0
+        STR     R5, [R4]                        ; clear reg clear count bit
 
         AND     R4, #0
         AND     R5, #0
-        AND     R6, #0
 
     ; START THE TIMER
         LDR     R4, STCTRL
         LDR     R5, [R4]
-        ORR     R5, #1
+        ORR     R5, #1                          ; sets enable bit of STCTRL
         STR     R5, [R4]
 
+        AND     R4, #0
+        AND     R5, #0
         LDR     R6, COUNTFLAG
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; THIS PART NEEDS WORK
 waitLoop:
         LDR     R4, STCTRL
-        LDR     R5, [R5]
+        LDR     R5, [R4]
         CMP     R5, R6
-        BEQ waitLoop
+        BEQ     waitLoop                        ; If not Countflag waitloop
         BX LR
 
         .endasmfunc
