d768853fe8d19c0e109f2c7a422ebf7831988682
James Hicks <97328702+Fireinthesky1@users.noreply.github.com>
2023-10-07 13:14:14 -0700

    Fixed spacing on lab1 added comments

--- lab1.asm
+++ lab1.asm
@@ -13,43 +13,44 @@
 
         .thumb
         .global main
-a_p: .word 0x200004F0 ; array pointer
-cnt: .equ 10          ; size of the array in bytes
+a_p: .word 0x200004F0 ; array pointer (field)
+cnt: .equ 10          ; size of the array in bytes (constant)
 main:
         .asmfunc
     ; here we initialize the registers
-        AND         R0, #0            ; clear R0 (loop index)
-        AND         R1, #0            ; clear R1 (array pointer)
-        AND         R2, #0            ; clear R2 (holds values to be stored)
-        AND         R3, #0            ; clear R3 (holds the sum)
+        AND         R0, #0        ; clear R0 (loop index)
+        AND         R1, #0        ; clear R1 (array pointer)
+        AND         R2, #0        ; clear R2 (holds values to be stored)
+        AND         R3, #0        ; clear R3 (holds the sum)
 
-        LDR         R1, a_p           ; store the array pointer into R1
+        LDR         R1, a_p       ; store the array pointer into R1
 
-        MOV         R2, #10           ; move 10 into the low bits of R2
+        MOV         R2, #10       ; move 10 into the low bits of R2
 
     ; this loop initializes the array
 loop_i:
-        STRB        R2, [R1], #1      ; *R1=R2, R1=R1+1 (1 byte further down)
-        ADD         R2, #3            ; R2=R2+3
-        ADD         R0, #1            ; increment R0 (the loop index)
-        CMP         R0, #cnt
-        BNE         loop_i
+        STRB        R2, [R1], #1  ; *R1=R2, R1=R1+1 (1 byte further)
+        ADD         R2, #3        ; R2=R2+3
+        ADD         R0, #1        ; increment R0 (the loop index)
+        CMP         R0, #cnt      ; compare index to total loops needed
+        BNE         loop_i        ; if not equal go back to label loop_i
 
     ; here we reset the register for the sum loop
-        LDR         R1, a_p           ; store the array pointer into R1 again
-        AND         R0, #0            ; clear loop index
-        AND         R2, #0            ; clear R2
+        LDR         R1, a_p       ; store the array pointer into R1 again
+        AND         R0, #0        ; clear loop index
+        AND         R2, #0        ; clear R2
 
     ; this loop calculates the sum
 loop_s:
-        LDRB        R2, [R1]          ; load the value pointed to by R1 into R2
-        ADD         R3, R2            ; add R2 to R3
-        ADD         R1, #1            ; R1 = R1 + 1
-        ADD         R0, #1            ; increment loop index
-        CMP         R0, #cnt
-        BNE         loop_s
+        LDRB        R2, [R1]      ; load the value pointed to by R1 into R2
+        ADD         R3, R2        ; add R2 to R3
+        ADD         R1, #1        ; R1 = R1 + 1
+        ADD         R0, #1        ; increment loop index
+        CMP         R0, #cnt      ; compare index to total loops needed
+        BNE         loop_s        ; if not equal go back to label loop_s
     ; R3 now holds 0x000000EB
-loop:	mov			R0, R0
-		B			loop
+    ; this is an infinite loop
+loop:   mov         R0, R0        ; move low 16 bits of R0 into R0
+        B           loop          ; go back to label loop
         .endasmfunc
         .end
