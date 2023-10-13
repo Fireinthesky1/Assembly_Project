aa25468157690c07bab4290d793187d372d6acbe
James Hicks <97328702+Fireinthesky1@users.noreply.github.com>
2023-10-13 11:22:47 -0700

    Got A Bit of the way through Lab 5

---
+++ display.h
@@ -0,0 +1,17 @@
+// DISPLAY HEADER FILE
+
+#pragma once
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <math.h>
+#include <inc/hw_memmap.h>
+#include <driverlib/gpio.h>
+#include <driverlib/sysctl.h>
+
+extern uint8_t number_to_display;
+extern uint8_t digit_to_display;
+
+uint8_t control_converter(void);
+uint8_t display_converter(void);
+void display_interrupt_handler(void);
