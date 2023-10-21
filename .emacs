d23d679691e15b98a95e859d367ef8c5ec7de510
James Hicks <97328702+Fireinthesky1@users.noreply.github.com>
2023-10-20 20:38:51 -0700

    Getting Close With The Interrupts Practice (debouncer ain't working)

---
+++ .emacs
@@ -0,0 +1,124 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(gruvbox-dark-soft))
+ '(custom-safe-themes
+   '("b1a691bb67bd8bd85b76998caf2386c9a7b2ac98a116534071364ed6489b695d" "2ff9ac386eac4dffd77a33e93b0c8236bb376c5a5df62e36d4bfa821d56e4e20" "72ed8b6bffe0bfa8d097810649fd57d2b598deef47c992920aef8b5d9599eefe" "d80952c58cf1b06d936b1392c38230b74ae1a2a6729594770762dc0779ac66b7" default))
+ '(package-selected-packages '(gruvbox-theme)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+; Stop Emacs from losing undo information by
+; setting very high limits for undo buffers
+(setq undo-limit 20000000)
+(setq undo-strong-limit 40000000)
+
+;; Whitespace customization
+(setq whitespace-style '(face tabs spaces space-mark lines-tail))
+(setq whitespace-display-mappings
+      '((newline-mark 0 [8729 10])
+	(space-mark 32 [183] [46])
+	(tab-mark 9 [9655 9] [92 9])))
+(global-whitespace-mode 1)
+
+;; use space instead of tabs
+(setq-default indent-tabs-mode nil)
+
+; 4-space tabs
+(setq tab-width 4)
+(setq indent-line-function 'insert-tab)
+
+;; set whitespace mode
+(global-whitespace-mode 1)
+
+;; Enable the visual line mode
+(global-visual-line-mode 1)
+
+;; Set the visual line mode to wraps lines at 80
+(setq visual-line-fringe-indicators '(nil right-curly-arrow))
+(setq visual-line-column 76)
+
+(setq whitespace-line-column 76)
+
+; no screwing with my middle mouse button
+(global-unset-key [mouse-2])
+
+; Startup windowing
+(setq next-line-add-newlines nil)
+(setq-default truncate-lines t)
+(setq truncate-partial-width-windows nil)
+(split-window-horizontally)
+
+;; Enable line numbers
+(global-display-line-numbers-mode t)
+
+;; move text up and down
+(defun move-text-internal (arg)
+  (cond
+   ((and mark-active transient-mark-mode)
+    (if (> (point) (mark))
+        (exchange-point-and-mark))
+    (let ((column (current-column))
+          (text (delete-and-extract-region (point) (mark))))
+      (forward-line arg)
+      (move-to-column column t)
+      (set-mark (point))
+      (insert text)
+      (exchange-point-and-mark)
+      (setq deactivate-mark nil)))
+   (t
+    (let ((column (current-column)))
+      (beginning-of-line)
+      (when (or (> arg 0) (not (bobp)))
+        (forward-line)
+        (when (or (< arg 0) (not (eobp)))
+          (transpose-lines arg))
+        (forward-line -1))
+      (move-to-column column t)))))
+
+(defun move-text-down (arg)
+  "Move region (transient-mark-mode active) or current line
+  arg lines down."
+  (interactive "*p")
+  (move-text-internal arg))
+
+(defun move-text-up (arg)
+  "Move region (transient-mark-mode active) or current line
+  arg lines up."
+  (interactive "*p")
+  (move-text-internal (- arg)))
+
+(provide 'move-text)
+
+
+(global-set-key [M-up] 'move-text-up)
+(global-set-key [M-down] 'move-text-down)
+
+; Bright-red TODOs
+;; Define a font-lock rule to highlight comments containing TODO in C/C++
+(font-lock-add-keywords
+ 'c++-mode
+ '(("\\<\\(TODO\\)\\>" 1 font-lock-warning-face t)))
+
+;; You can also add the rule to the generic c-mode
+(font-lock-add-keywords
+ 'c-mode
+ '(("\\<\\(TODO\\)\\>" 1 font-lock-warning-face t)))
+
+;; Define a font-lock rule to highlight comments containing TODO in assembly
+(add-hook 'asm-mode-hook
+          (lambda ()
+            (font-lock-add-keywords
+             nil
+             '(("\\<\\(TODO\\)\\>" 1 font-lock-warning-face t)))))
+
+; No hungry backspace
+(c-toggle-auto-hungry-state -1)
+
+
--- interrupts_practice.c
+++ interrupts_practice.c
@@ -2,7 +2,7 @@
 // PROBLEM 2
 // James Hicks
 
-#include <stdool.h>
+#include <stdbool.h>
 #include <stdint.h>
 #include <inc/hw_memmap.h>
 #include <inc/hw_ints.h>
@@ -11,13 +11,19 @@
 #include <driverlib/timer.h>
 #include <driverlib/interrupt.h>
 
-uint8_t LED_RED = 0x2;
-uint8_t LED_RED_BLUE = 0x6;
-uint8_t LED_BLUE = 0x4;
-uint8_t LED_GREEN = 0x8;
-uint8_t LED_RED_GREEN_BLUE = 0xE;
+const uint8_t LED_RED            = 0x02; // one
+const uint8_t LED_RED_BLUE       = 0x06; // two
+const uint8_t LED_BLUE           = 0x04; // three
+const uint8_t LED_GREEN          = 0x08; // four
+const uint8_t LED_RED_GREEN_BLUE = 0x0E; // five
 
+uint8_t current_state = 0x11;
+uint8_t current_color = 0x02;
 
+uint8_t none = 0x00000011;
+uint8_t one  = 0x00000001;
+uint8_t two  = 0x00000010;
+uint8_t both = 0x00000000;
 
 // 1) disable timer interrupts
 // 2) clear timer interrupt flag
@@ -34,7 +40,84 @@ void timer1_interrupt_handler(void)
    TimerIntClear(TIMER1_BASE,
                  TIMER_TIMA_TIMEOUT);
 
+   uint8_t input = GPIOPinRead(GPIO_PORTF_BASE,
+                               GPIO_PIN_0 | GPIO_PIN_4);
+
    // STATE TRANSITIONS
+   switch(current_color)
+   {
+
+      case LED_RED:
+         if(input == 0x01)             // sw 1 (back)
+         {
+            current_color = LED_RED_GREEN_BLUE;
+         }
+         else if(input == 0x10)        // sw2 (forward)
+         {
+            current_color = LED_RED_BLUE;
+         }
+         break;
+
+      case LED_RED_BLUE:
+         if(input == 0x01)             // sw 1 (back)
+         {
+            current_color = LED_RED;
+         }
+         else if(input == 0x10)        // sw2 (forward)
+         {
+            current_color = LED_BLUE;
+         }
+         break;
+
+      case LED_BLUE:
+         if(input == 0x01)             // sw 1 (back)
+         {
+            current_color = LED_RED_BLUE;
+         }
+         else if(input == 0x10)        // sw2 (forward)
+         {
+            current_color = LED_GREEN;
+         }
+         break;
+
+      case LED_GREEN:
+         if(input == 0x01)             // sw 1 (back)
+         {
+            current_color = LED_BLUE;
+         }
+         else if(input == 0x10)        // sw2 (forward)
+         {
+            current_color = LED_RED_GREEN_BLUE;
+         }
+         break;
+
+      case LED_RED_GREEN_BLUE:
+         if(input == 0x01)             // sw 1 (back)
+         {
+            current_color = LED_GREEN;
+         }
+         else if(input == 0x10)        // sw2 (forward)
+         {
+            current_color = LED_RED;
+         }
+         break;
+
+      default:                           // bad if we get here
+         break;
+   }
+
+   current_state = input;
+
+   // WRITE THE CURRENT STATE
+   GPIOPinWrite(GPIO_PORTF_BASE,
+                GPIO_PIN_1 | GPIO_PIN_2 |
+                GPIO_PIN_3,
+                current_color);
+
+   // ENABLE GPIO INTERRUPTS
+   GPIOIntEnable(GPIO_PORTF_BASE,
+                 GPIO_PIN_0 | GPIO_PIN_4);
+
 }
 
 // 1) disable portf interrupts
@@ -59,7 +142,7 @@ void portf_interrupt_handler(void)
    // LOAD THE TIMER (.010 seconds)
    TimerLoadSet(TIMER1_BASE,
                 TIMER_A,
-                0x27100);
+                0x4E200); // 27100 = .01 sec
 
    // START TIMER1
    TimerEnable(TIMER1_BASE,
@@ -85,19 +168,19 @@ void gpio_init(void)
 
    // UNLOCK PF0
    GPIOUnlockPin(GPIO_PORTF_BASE,
-                 GPIO_PIN_0);
+                 GPIO_PIN_0 | GPIO_PIN_4);
 
    // ENABLE PUR FOR SWITCHES
    GPIOPadConfigSet(GPIO_PORTF_BASE,
                     GPIO_PIN_0 | GPIO_PIN_4,
                     GPIO_STRENGTH_2MA,
-                    GPIO_PIN_TYPE_STD);
+                    GPIO_PIN_TYPE_STD_WPU);
 
    // SET DRIVE STRENGTH FOR LEDS
    GPIOPadConfigSet(GPIO_PORTF_BASE,
                     GPIO_PIN_1 | GPIO_PIN_2 |
                     GPIO_PIN_3,
-                    GPIO_STRENGTH_2MA
+                    GPIO_STRENGTH_2MA,
                     GPIO_PIN_TYPE_STD);
 
    // USE THE 16Mhz PIOSC
@@ -109,6 +192,11 @@ void gpio_init(void)
    // DISABLE PRIORITY MASKING
    IntPriorityMaskSet(0x0);
 
+   // ONLY TRIGGER ON FALLING EDGE
+   GPIOIntTypeSet(GPIO_PORTF_BASE,
+                  GPIO_PIN_0 | GPIO_PIN_4,
+                  GPIO_FALLING_EDGE);
+
    // ENABLE INTERRUPTS FOR PF0 and PF4
    GPIOIntEnable(GPIO_PORTF_BASE,
                  GPIO_INT_PIN_0 | GPIO_INT_PIN_4);
@@ -116,11 +204,6 @@ void gpio_init(void)
    // REGISTER THE HANDLER
    GPIOIntRegister(GPIO_PORTF_BASE,
                    portf_interrupt_handler);
-
-   // ONLY TRIGGER ON FALLING EDGE
-   GPIOIntTypeSet(GPIO_PORTF_BASE,
-                  GPIO_PIN_0 | GPIO_PIN_4,
-                  GPIO_FALLING_EDGE);
 
 }
 
@@ -149,16 +232,16 @@ void timer1_init(void)
 
    // 16 Mhz 16,000,000 cycles per second ; .010 seconds
    TimerLoadSet(TIMER1_BASE,
-                TIMERA,
-                0x27100x);
+                TIMER_A,
+                0x27100);
 
    // INTERRUPT TRIGGERS ON TIMEOUT
    TimerIntEnable(TIMER1_BASE,
-                  TIMER_TIMEA_TIMEOUT);
+                  TIMER_TIMA_TIMEOUT);
 
    // REGISTER THE HANDLER
    TimerIntRegister(TIMER1_BASE,
-                    TIMERA,
+                    TIMER_A,
                     timer1_interrupt_handler);
 
 }
@@ -168,13 +251,16 @@ int main(void)
 {
 
    // INITIALIZE GPIO
-   gpio_init(void);
+   gpio_init();
 
    // INITIALIZE TIMER
-   timer1_init(void);
+   timer1_init();
 
    // SET INITIAL STATE FOR LED
-   GPIODataWrite();
+   GPIOPinWrite(GPIO_PORTF_BASE,
+                GPIO_PIN_1 | GPIO_PIN_2 |
+                GPIO_PIN_3,
+                current_color);
 
    while(1)
    {
