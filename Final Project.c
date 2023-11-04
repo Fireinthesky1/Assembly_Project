53b0e8d09e70e8ee8a83c55a6ad9e5e3f439dc1a
James Hicks <97328702+Fireinthesky1@users.noreply.github.com>
2023-11-04 14:41:26 -0700

    Begun Final Project

---
+++ Final Project.c
@@ -0,0 +1,47 @@
+// FINAL PROJECT
+// JAMES HICKS
+// 11/3/2023
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <inc/hw_memmap.h>
+#include <inc/hw_ints.h>
+#include <driverlib/gpio.h>
+#include <driverlib/sysctl.h>
+#include <driverlib/timer.h>
+
+int adc_init(void)
+{
+    
+}
+
+int gpio_init(void)
+{
+    
+}
+
+int timer1_init(void)
+{
+    
+}
+
+int timer2_init(void)
+{
+    
+}
+
+int main(void)
+{
+    // INITIALIZE GPIO
+
+    // INITIALIZE TIMERS
+
+    // INITIALIZE ADCS
+
+    // START TIMERS
+
+    // LOOP FOREVER
+    while(1)
+    {
+    }
+}
